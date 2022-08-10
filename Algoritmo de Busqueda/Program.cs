037ef12ea9e39882b430e233d7c2050db419c811
David <David@KBAIOS-DESKTOP>
2022-08-09 21:16:43 -0600

    Add project files.

---
+++ Algoritmo de Busqueda/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+namespace Algoritmo_de_Busqueda
+{
+    internal class Program
+    {
+        public static Stopwatch stopwatch = new Stopwatch();
+        public static List<int> randNumList = new List<int>();
+        public static Random random = new Random();
+
+
+
+        static void Main(string[] args)
+        {
+
+            stopwatch.Start();
+            RandomNumlist(40);
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.Elapsed.ToString());
+
+            foreach(int number in randNumList)
+            {
+                Console.Write(number + ", ");
+            }
+
+        }
+        public static void RandomNumlist(int MaxNums)
+        {
+            for (int i = 0; i < MaxNums; i++)
+            {
+                int RandomNum;
+                do RandomNum = random.Next(0,1000);
+                while (randNumList.Contains(RandomNum));
+                randNumList.Add(RandomNum);
+            }
+        }
+    }
+}
