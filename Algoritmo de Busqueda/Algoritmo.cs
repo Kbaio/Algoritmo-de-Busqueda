6b2e0653e0e1a9a889f31421c42c2e723ff8ad47
David <David@KBAIOS-DESKTOP>
2022-08-10 19:22:46 -0600

    Algoritmo con pruebas

---
+++ Algoritmo de Busqueda/Algoritmo.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritmo_de_Busqueda
+{
+    internal class Algoritmo
+    {
+        public List<int> randNumList = new List<int>();
+        public Random random = new Random();
+        public int maxNums = 10000;
+ 
+
+        /// <summary>
+        /// Esta funcion se encarga de definir el numero a buscar y recorre la lista de numeros
+        /// en busca del numero definido.
+        /// </summary>
+        public void FindRandomNumFirst()
+        {
+            int searchNum = random.Next(0, maxNums);
+            foreach (int number in randNumList)
+            {
+                if (number == searchNum)
+                {
+                    Console.WriteLine("Se encontró el valor "+ searchNum +" en la iteración " + randNumList.IndexOf(number));
+                    break;
+                }
+            }
+        }
+
+        public void FindRandomNumSecond()
+        {
+            int searchNum = random.Next(0, maxNums);
+            int start = 0;
+            int end = maxNums-1;
+            for(int i = 0; i < maxNums/2;i++)
+            {
+                if (randNumList[start] == searchNum) {
+                    Console.WriteLine("Se encontró el valor "+ searchNum +" en la iteración " + i + " en la posicion " + start);
+                    break;
+                }else if (randNumList[end] == searchNum)
+                {
+                    Console.WriteLine("Se encontró el valor " + searchNum + " en la iteración " + i + " en la posicion " + end);
+                    break;
+                }
+                else
+                {
+                    start++;
+                    end--;
+                }
+            }
+
+
+        }
+
+        /// <summary>
+        /// Esta funcion se encarga de generar numeros random y los agrega a la lista randNumList, adicionalmente se 
+        /// veriica que los numeros no se repitan.
+        /// </summary>
+        public void GenerateRandomNumlist()
+        {
+            for (int i = 0; i < maxNums; i++)
+            {
+                int RandomNum;
+                do RandomNum = random.Next(0, maxNums);
+                while (randNumList.Contains(RandomNum));
+                randNumList.Add(RandomNum);
+            }
+        }
+
+    }
+}
--- Algoritmo de Busqueda/Program.cs
+++ Algoritmo de Busqueda/Program.cs
@@ -7,34 +7,42 @@ namespace Algoritmo_de_Busqueda
     internal class Program
     {
         public static Stopwatch stopwatch = new Stopwatch();
-        public static List<int> randNumList = new List<int>();
-        public static Random random = new Random();
-
+        public static Algoritmo algoritmo = new Algoritmo();
 
 
         static void Main(string[] args)
         {
-
+            algoritmo.GenerateRandomNumlist();
             stopwatch.Start();
-            RandomNumlist(40);
+            algoritmo.FindRandomNumFirst();
             stopwatch.Stop();
             Console.WriteLine(stopwatch.Elapsed.ToString());
-
-            foreach(int number in randNumList)
-            {
-                Console.Write(number + ", ");
-            }
+            stopwatch.Reset();
 
         }
-        public static void RandomNumlist(int MaxNums)
-        {
-            for (int i = 0; i < MaxNums; i++)
-            {
-                int RandomNum;
-                do RandomNum = random.Next(0,1000);
-                while (randNumList.Contains(RandomNum));
-                randNumList.Add(RandomNum);
-            }
-        }
+
+        /*Resultados de FindRandomNumFirst algoritmo, este algoritmo recorre la lista compradando cada elemento hasta encontrar el indicado
+         * se estara haciendo la prueba con una lista de 10000 numeros aleatorios
+         * 1.Se encontró el valor 1269 en la iteración 9552 y el tiempo fue de 00:00:00.0006855
+         * 2.Se encontró el valor 4229 en la iteración 750 y el tiempo fue de 00:00:00.0005589
+         * 3.Se encontró el valor 520 en la iteración 5056 y el tiempo fue de 00:00:00.0005834
+         * 4.Se encontró el valor 1906 en la iteración 6766 y el tiempo fue de 00:00:00.0005701
+         * 5.Se encontró el valor 7270 en la iteración 5025 y el tiempo fue de 00:00:00.0005254
+         * 6.Se encontró el valor 7539 en la iteración 8175 y el tiempo fue de 00:00:00.0005451
+         * 7.Se encontró el valor 1382 en la iteración 41 y el tiempo fue de 00:00:00.0005661
+         * 8.Se encontró el valor 9666 en la iteración 4157 y el tiempo fue de 00:00:00.0005709
+         * 9.Se encontró el valor 2358 en la iteración 3579 y el tiempo fue de 00:00:00.0006209
+         * 10.Se encontró el valor 9936 en la iteración 4769 y el tiempo fue de 00:00:00.0005777
+         * 
+         * 
+         * Mejor de los casos: El mejor de los casos fue la corrida 7 en la cualse hicieron un total de 41 iteraciones
+         * 
+         * De las iteraciones obtenidas obtenidos el promedio es de: 4787
+         * 
+         * El peor de los casos fur el primer caso el cual tuvo un total de 9552 iteraciones.
+        */
+
+
+
     }
 }
